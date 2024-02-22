ae4070ebf12e70cc92b3ee8f3f99bdbb2f239e80
kylecreekmore <kylecreekmore@gmail.com>
2024-02-21 22:05:10 -0500

    Add simple layout

--- parkway.admin/src/App.tsx
+++ parkway.admin/src/App.tsx
@@ -1,10 +1,30 @@
-import './App.css';
 import { App as AntdApp } from 'antd';
+import styles from './App.module.css';
 
 function App() {
   return (
     <AntdApp>
-      <h1>Parkway Ministries Admin</h1>
+      <div className={styles.mainContainer}>
+        <header>
+          <h1>Parkway Ministries Admin</h1>
+        </header>
+        <div className={styles.container}>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/directory">Directory</a>
+              </li>
+              <li>
+                <a href="/giving">Giving</a>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.mainContent}></div>
+        </div>
+      </div>
     </AntdApp>
   );
 }
