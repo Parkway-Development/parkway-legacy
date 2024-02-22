b9ec9eb525699ab2d779054f688365c833c3a46a
kylecreekmore <kylecreekmore@gmail.com>
2024-02-21 20:52:18 -0500

    Add eslint and prettier

--- parkway.admin/.eslintrc.cjs
+++ parkway.admin/.eslintrc.cjs
@@ -3,16 +3,36 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json']
+  },
+  plugins: ['react-refresh', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  settings: {
+    react: {
+      createClass: "createReactClass", // Regex for Component Factory to use,
+                                         // default to "createReactClass"
+      pragma: "React",  // Pragma to use, default to "React"
+      fragment: "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      version: "detect", // React version. "detect" automatically picks the version you have installed.
+                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                           // It will default to "latest" and warn if missing, and to "detect" in the future
+      flowVersion: "0.53" // Flow version
+    }
+  }
 }
