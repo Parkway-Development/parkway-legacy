18b74fc9bb7591bc4a01765019f709c60c723fde
jaydavis <jay@jaydavis.us>
2024-02-23 14:57:25 -0500

    Signup process completed using bcrypt

--- parkway.api/controllers/userController.js
+++ parkway.api/controllers/userController.js
@@ -7,7 +7,15 @@ const loginUser = async (req, res) => {
 
 //connect user
 const signupUser = async (req, res) => {
-    res.json({mssg: "signup user"})
+    const {email, password} = req.body
+    try {
+        const user = await User.signup(email, password)
+        res.status(201).json({email, user})
+    } catch (err) {
+        res.status(400).json({err: err.message})
+    }
 }
+
+//profile
 
 module.exports = { signupUser, loginUser }
--- parkway.api/models/userModel.js
+++ parkway.api/models/userModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
-    username: {
+    email: {
         required: true,
         type: String,
         unique: true
@@ -14,10 +15,21 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         required: false,
         type: String
-    },
-    date: {
-        type: Date,
-        default: Date.now()
     }
 }, {timestamps: true})
+
+//static signup method
+userSchema.statics.signup = async function(email, password) {
+
+    const exists = await this.findOne({email})
+    if(exists) {
+        throw new Error('Email already exists')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    const user = await this.create({email, password: hash})
+    return user
+}
+
 module.exports = mongoose.model('User', userSchema, 'users')
