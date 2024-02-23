db9208c8091cfd5cf85541d57dfe8e4e04a2a85f
kylecreekmore <kylecreekmore@gmail.com>
2024-02-22 21:19:14 -0500

    Add logic to load data on directory page

--- parkway.admin/src/components/directory-page/DirectoryPage.tsx
+++ parkway.admin/src/components/directory-page/DirectoryPage.tsx
@@ -1,5 +1,58 @@
+import useAxios from '../../hooks/useAxios.ts';
+import { Alert, Empty, Spin, Table } from 'antd';
+import { DirectoryUser } from '../../types/DirectoryUser.ts';
+import { ColumnsType } from 'antd/lib/table';
+import styles from './DirectoryPage.module.css';
+
 const DirectoryPage = () => {
-  return <h2>Directory</h2>;
+  return (
+    <>
+      <h2>Directory</h2>
+      <DirectoryList />
+    </>
+  );
+};
+
+const directoryListColumns: ColumnsType<DirectoryUser> = [
+  {
+    title: 'First Name',
+    dataIndex: 'firstname',
+    key: 'firstname'
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'lastname',
+    key: 'lastname'
+  },
+  {
+    title: 'Mobile',
+    dataIndex: 'mobile',
+    key: 'mobile'
+  }
+];
+
+const DirectoryList = () => {
+  const { loading, error, data } =
+    useAxios<DirectoryUser[]>('/api/people/getall');
+
+  if (error) {
+    return <Alert type="error" message={error} />;
+  }
+
+  if (loading) {
+    return <Spin />;
+  }
+
+  if (!data?.length) {
+    return <Empty />;
+  }
+
+  return (
+    <div className={styles.dataContainer}>
+      <p>Total Records {data.length}</p>
+      <Table dataSource={data} columns={directoryListColumns} />
+    </div>
+  );
 };
 
 export default DirectoryPage;
---
+++ parkway.admin/src/types/DirectoryUser.ts
@@ -0,0 +1,5 @@
+export interface DirectoryUser {
+  firstname: string;
+  lastname: string;
+  mobile: string;
+}
