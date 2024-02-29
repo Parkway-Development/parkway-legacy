a9c2a04ffb5fb83eca0c4febc7c4a544ec66f940
jaydavis <jay@jaydavis.us>
2024-02-29 13:56:07 -0500

    initial scaffold of liabilityModel

--- parkway.api/controllers/accountingController.js
+++ parkway.api/controllers/accountingController.js
@@ -7,6 +7,7 @@ const Payroll = require('../models/payrollModel');
 const Fund = require('../models/fundModel');
 const Budget = require('../models/budgetModel');
 const Asset = require('../models/assetModel');
+const Liability = require('../models/liabilityModel');
 
 //Donations
 //Post a donation
@@ -731,8 +732,81 @@ const deleteAsset = async (req, res) => {
     }
 }
 
+//Liabilities
+//Post a liability
+const addLiability = async (req, res) => {
+    try {
+        const liability = new Liability(req.body);
+        await liability.save();
+        res.status(201).send(liability);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
 
+//Get all liabilities
+const getAllLiabilities = async (req, res) => {
+    try {
+        const liabilities = await Liability.find({});
+        res.status(200).send(liabilities);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
 
+//Get liability by ID
+const getLiabilityById = async (req, res) => {
+    try {
+        const liability = await Liability.findById(req.params.id);
+        res.status(200).send(liability);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+//Get liabilities by fund
+const getLiabilitiesByFund = async (req, res) => {
+    try {
+        const liabilities = await Liability.find({ fund: req.params.id });
+        res.status(200).send(liabilities);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+//Get liabilities by category
+const getLiabilitiesByCategory = async (req, res) => {
+    try {
+        const liabilities = await Liability.find({ category: req.params.category });
+        res.status(200).send(liabilities);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+//Update a liability by ID
+const updateLiability = async (req, res) => {
+    try {
+        await Liability.findByIdAndUpdate
+            (req.params.id, req.body
+                , { new: true, runValidators: true }
+            );
+        res.status(200).send('Liability updated');
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+//Delete a liability by ID
+const deleteLiability = async (req, res) => {
+    try {
+        await Liability.findByIdAndDelete(req.params.id);
+        res.status(200).send('Liability deleted');
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
 
 
 
@@ -788,5 +862,12 @@ module.exports = {
     getAssetsByCategory,
     getAssetsByLocation,
     updateAsset,
-    deleteAsset
+    deleteAsset,
+    addLiability,
+    getAllLiabilities,
+    getLiabilityById,
+    getLiabilitiesByFund,
+    getLiabilitiesByCategory,
+    updateLiability,
+    deleteLiability
 }
