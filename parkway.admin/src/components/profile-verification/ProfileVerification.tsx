b2be6c98c8e810d0c06b9337b6e84a5b69241301
kylecreekmore <kylecreekmore@gmail.com>
2024-03-05 22:37:47 -0500

    Work in progress for profile verification

--- parkway.admin/src/components/profile-verification/ProfileVerification.tsx
+++ parkway.admin/src/components/profile-verification/ProfileVerification.tsx
@@ -1,27 +1,140 @@
 import { InternalLoginResponse, useAuth } from '../../hooks/useAuth.tsx';
 import styles from './ProfileVerification.module.css';
-import { Alert, Card, Spin } from 'antd';
-import { ReactNode, useEffect } from 'react';
+import { Alert, Button, Card, Input, Spin } from 'antd';
+import {
+  ReactNode,
+  SyntheticEvent,
+  useCallback,
+  useEffect,
+  useState
+} from 'react';
 import { useMutation } from '@tanstack/react-query';
 import useApi from '../../hooks/useApi.ts';
 import { useNavigate } from 'react-router-dom';
-import UserProfileForm, {
+import {
   addProfileInitialValues,
   transformFieldsToMyProfilePayload,
   transformFieldsToPayload,
   UserProfileFormFields
 } from '../user-profile-page/UserProfileForm.tsx';
 import { UserProfile } from '../../types/UserProfile.ts';
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 
 type ProfileVerificationProps = {
   loginResponse: InternalLoginResponse;
 };
 
+type InputComparison = {
+  placeholder: string;
+  field: keyof UserProfile;
+  input?: string;
+  compareFn: (input?: string) => boolean;
+  isValid?: boolean;
+};
+
+const stringComparison = (
+  input: string | undefined,
+  expected: string,
+  isPhoneNumber: boolean = false
+) => {
+  if (expected === input) return true;
+  if (!input?.trim().length) return false;
+
+  if (isPhoneNumber) {
+    return input.replace(/[^0-9]/g, '') === expected.replace(/[^0-9]/g, '');
+  }
+
+  return input.trim().toLowerCase() === expected.trim().toLowerCase();
+};
+
 const ProfileVerification = ({ loginResponse }: ProfileVerificationProps) => {
   console.log('login response', loginResponse);
   const navigate = useNavigate();
-  const { logout, storeProfileId } = useAuth();
+  const { storeProfileId } = useAuth();
   const { errorMessage, profile, user } = loginResponse;
+  const [inputComparisons, setInputComparison] = useState<InputComparison[]>(
+    () => {
+      if (!profile) return [];
+
+      const result: InputComparison[] = [];
+
+      if (profile.firstName) {
+        result.push({
+          placeholder: 'First Name',
+          field: 'firstName',
+          compareFn: (input) => stringComparison(input, profile.firstName)
+        });
+      }
+
+      if (profile.lastName) {
+        result.push({
+          placeholder: 'Last Name',
+          field: 'lastName',
+          compareFn: (input) => stringComparison(input, profile.lastName)
+        });
+      }
+
+      if (profile.mobilePhone) {
+        result.push({
+          placeholder: 'Mobile Phone',
+          field: 'mobilePhone',
+          compareFn: (input) =>
+            stringComparison(input, profile.mobilePhone!, true)
+        });
+      }
+
+      if (profile.homePhone) {
+        result.push({
+          placeholder: 'Home Phone',
+          field: 'homePhone',
+          compareFn: (input) =>
+            stringComparison(input, profile.homePhone!, true)
+        });
+      }
+
+      if (profile.streetAddress1) {
+        result.push({
+          placeholder: 'Address Line 1',
+          field: 'streetAddress1',
+          compareFn: (input) => stringComparison(input, profile.streetAddress1!)
+        });
+      }
+
+      if (profile.streetAddress2) {
+        result.push({
+          placeholder: 'Address Line 2',
+          field: 'streetAddress2',
+          compareFn: (input) => stringComparison(input, profile.streetAddress2!)
+        });
+      }
+
+      if (profile.city) {
+        result.push({
+          placeholder: 'City',
+          field: 'city',
+          compareFn: (input) => stringComparison(input, profile.city!)
+        });
+      }
+
+      if (profile.state) {
+        result.push({
+          placeholder: 'State',
+          field: 'state',
+          compareFn: (input) => stringComparison(input, profile.state!)
+        });
+      }
+
+      if (profile.zip) {
+        result.push({
+          placeholder: 'Zip',
+          field: 'zip',
+          compareFn: (input) => stringComparison(input, profile.zip!)
+        });
+      }
+
+      return result;
+    }
+  );
   const {
     createUserProfile,
     formatError,
@@ -85,7 +198,21 @@ const ProfileVerification = ({ loginResponse }: ProfileVerificationProps) => {
     }
   }, []);
 
-  const disableOptions = isJoining || isAdding || isUpdating;
+  const handleInputChange = useCallback(
+    (e: SyntheticEvent<HTMLInputElement>) => {
+      const newValue = e.currentTarget.value;
+      const fieldName = e.currentTarget.name;
+
+      setInputComparison((prev) =>
+        prev.map((i) =>
+          i.field === fieldName
+            ? { ...i, input: newValue, isValid: i.compareFn(newValue) }
+            : i
+        )
+      );
+    },
+    []
+  );
 
   let content: ReactNode;
 
@@ -108,10 +235,17 @@ const ProfileVerification = ({ loginResponse }: ProfileVerificationProps) => {
     content = <Alert className={styles.error} message={error} type="error" />;
   } else if (!profile) {
     content = (
-      <div className={styles.createProfile}>
-        <Spin size="large" />
-        <p>Creating your profile...</p>
-      </div>
+      <Card
+        className={styles.card}
+        title="Complete Registration"
+        bordered={false}
+        style={{ width: 500 }}
+      >
+        <div className={styles.createProfile}>
+          <Spin size="large" />
+          <p>Creating your profile...</p>
+        </div>
+      </Card>
     );
   } else {
     const handleUpdateUserProfile = (fields: UserProfileFormFields) => {
@@ -132,42 +266,76 @@ const ProfileVerification = ({ loginResponse }: ProfileVerificationProps) => {
       handleUpdateUserProfile({ ...profile, ...payload, email: user.email });
     };
 
+    const validInputs = inputComparisons.reduce(
+      (p, current) => (current.isValid ? p + 1 : p),
+      0
+    );
+    const canValidate =
+      validInputs === inputComparisons.length && validInputs > 0;
+
     content = (
-      <>
-        {profile && (
-          <p className={styles.email}>
-            Information was already found for your email address. Please enter
-            your information below.
+      <Card className={styles.verifyMatch}>
+        <div className={styles.header}>
+          <h2>Hey! We are glad you joined us!</h2>
+          <p>
+            We found a profile that seems to match your email address. Can you
+            tell us a bit more about yourself to see if it is a match?
           </p>
-        )}
-        <p className={styles.email}>Email: {user.email}</p>
-        <UserProfileForm
-          isSaving={disableOptions}
-          onFinish={handleSubmit}
-          onCancel={() => {
-            logout();
-            window.location.reload();
-          }}
-          submitText="Complete Registration"
-          cancelText="Cancel"
-          isMyProfile
-        />
-      </>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Your Input</th>
+              <th>Our Data</th>
+            </tr>
+          </thead>
+          <tbody>
+            {inputComparisons.map(({ placeholder, input, isValid, field }) => (
+              <tr key={field}>
+                <td>
+                  <Input
+                    placeholder={placeholder}
+                    value={input}
+                    onChange={handleInputChange}
+                    name={field}
+                    autoComplete="off"
+                  />
+                </td>
+                <td className={styles.validColumn}>
+                  {isValid ? (
+                    <CheckOutlined style={{ color: 'green' }} />
+                  ) : (
+                    <CloseOutlined style={{ color: 'red' }} />
+                  )}
+                  <Input
+                    readOnly
+                    tabIndex={-1}
+                    placeholder={
+                      isValid ? profile[field]?.toString() : '********'
+                    }
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <Button type="primary">Cancel</Button>
+              </td>
+              <td>
+                <Button type="primary" disabled={!canValidate}>
+                  Submit
+                </Button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </Card>
     );
   }
 
-  return (
-    <div className={styles.entryPage}>
-      <Card
-        className={styles.card}
-        title="Complete Registration"
-        bordered={false}
-        style={{ width: 500 }}
-      >
-        {content}
-      </Card>
-    </div>
-  );
+  return <div className={styles.entryPage}>{content}</div>;
 };
 
 export default ProfileVerification;
