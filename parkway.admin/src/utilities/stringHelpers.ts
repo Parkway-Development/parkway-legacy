54248b06ca991bfcf5afb03aca4419a8c7b088b9
kylecreekmore <kylecreekmore@gmail.com>
2024-03-11 21:15:45 -0400

    Improve string trim logic for dates

--- parkway.admin/src/utilities/stringHelpers.ts
+++ parkway.admin/src/utilities/stringHelpers.ts
@@ -1,14 +1,30 @@
+import dayjs from 'dayjs';
+
+const isValidDate = (date: any): boolean => {
+  if (date instanceof dayjs) return true;
+
+  return (
+    date &&
+    Object.prototype.toString.call(date) === '[object Date]' &&
+    !isNaN(date)
+  );
+};
+
 export const trimStrings = <T extends {}>(payload: T): T => {
   const trimmedPayload = { ...payload };
 
   for (const [key, value] of Object.entries(payload)) {
+    console.log('key value', key, value, typeof value, isValidDate(value));
     if (typeof value === 'string') {
       // @ts-ignore
       trimmedPayload[key] = value.trim();
+    } else if (isValidDate(value)) {
+      // do nothing
     } else if (Array.isArray(value)) {
       // @ts-ignore
       trimmedPayload[key] = value.map((item) => {
         if (typeof item === 'string') return item.trim();
+        if (isValidDate(item)) return item;
         if (typeof item === 'object') return trimStrings(item);
         return item;
       });
