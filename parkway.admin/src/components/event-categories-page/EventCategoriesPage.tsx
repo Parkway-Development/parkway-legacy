4e3c467f24548b61cdb08d07b60438fe1f81a3fb
kylecreekmore <kyle.creekmore@gmail.com>
2024-03-22 09:37:28 -0400

    Fix add event category button text

--- parkway.admin/src/components/event-categories-page/EventCategoriesPage.tsx
+++ parkway.admin/src/components/event-categories-page/EventCategoriesPage.tsx
@@ -19,6 +19,7 @@ const EventCategoriesPage = () => (
     baseApiType="eventCategoriesApi"
     columns={eventCategoryColumns}
     title="Event Categories"
+    addLinkTitle="Add Event Category"
   />
 );
 
