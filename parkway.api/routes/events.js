c77545a8f907d6308ce800138a00d06a9b33884b
kylecreekmore <kyle.creekmore@gmail.com>
2024-03-22 09:38:32 -0400

    Use base api routes for events

--- parkway.api/routes/events.js
+++ parkway.api/routes/events.js
@@ -6,28 +6,14 @@ const {
     getById,
     updateEvent,
     deleteEvent
-} = require('../controllers/eventController')
+} = require('../controllers/eventController');
+const { addNotFoundHandler, configureBaseApiRoutes } = require("./baseApiRouter");
 
 const { requireAuthorization} = require("../auth");
 requireAuthorization(router);
 
-//Post an event
-router.post('/', addEvent)
+configureBaseApiRoutes(router, addEvent, getAll, getById, updateEvent, deleteEvent);
 
-//Get all event
-router.get('/', getAll)
-
-//Get event by ID
-router.get('/:id', getById)
-
-//Update an event by id
-router.patch('/:id', updateEvent)
-
-//Delete event by id
-router.delete('/:id', deleteEvent)
-
-router.use('*', (req, res) => {
-    res.status(404).json({ message: 'Not Found' });
-});
+addNotFoundHandler(router);
 
 module.exports = router;
