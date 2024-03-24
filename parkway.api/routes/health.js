01c13908d43ce6f7989d5824f558668f1cc7bedf
jaydavis <jay@jaydavis.us>
2024-03-24 16:13:36 -0400

    finished with health checks

--- parkway.api/routes/health.js
+++ parkway.api/routes/health.js
@@ -1,8 +1,16 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ message: "API is healthy." });
+
+    const dbState = mongoose.connection.readyState;
+    if(dbState === 1) {
+        res.status(200).json({ databaseStatus: 'Connected', apiStatus: 'Healthy' });
+    } else {
+        // Respond with a server error status code if not connected
+        res.status(500).json({ message: 'Internal Server Error', dbStatus: 'Disconnected' });
+    }
 });
 
 
