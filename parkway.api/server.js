fc8823159b8d28a7ffaec50f7a715695d2d989c3
jaydavis <jay@jaydavis.us>
2024-03-24 16:34:07 -0400

    fixed launch.json

--- parkway.api/server.js
+++ parkway.api/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const Multer = require('multer');
 const conn = mongoose.connection;
+const Multer = require('multer');
 const upload = Multer({ dest: 'uploads/' });
 
 const userRoutes = require('./routes/users');
