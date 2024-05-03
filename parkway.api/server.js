20559ce714a3050da9e860f327098fc1ef2dd470
kylecreekmore <kylecreekmore@gmail.com>
2024-05-03 17:39:06 -0400

    Update readme and cors handling

--- parkway.api/server.js
+++ parkway.api/server.js
@@ -39,10 +39,10 @@ if (process.env.ALLOWED_ORIGINS) {
 
     const corsOptions = {
         origin: (origin, callback) => {
-            if (allowedOrigins.includes(origin)) {
+            if (allowedOrigins.includes(origin) || !origin) {
                 callback(null, true);
             } else {
-                callback(new Error('Not allowed by CORS'));
+                callback(new Error(`Not allowed by CORS for origin [${origin}]`));
             }
         },
     };
