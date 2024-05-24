b615460d0baf3d20bd337bd72ecba92adb4956ad
kylecreekmore <kylecreekmore@gmail.com>
2024-05-24 15:30:29 -0400

    Add react eslint configs

--- parkway.admin/eslint.config.js
+++ parkway.admin/eslint.config.js
@@ -2,6 +2,9 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -9,5 +12,26 @@ export default tseslint.config(
   ...tseslint.configs.stylistic,
   {
     ignores: ['node_modules/', 'dist/', '.prettierrc.cjs']
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect' // Automatically detect the version of React to use
+      }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y
+    },
+    rules: {
+      'react/jsx-uses-react': 'off', // Not needed with React 17+ (new JSX transform)
+      'react/react-in-jsx-scope': 'off', // Not needed with React 17+ (new JSX transform)
+      'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
+      'jsx-a11y/no-static-element-interactions': 'error', // Example of a custom rule setting
+      'jsx-a11y/click-events-have-key-events': 'error',
+      'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+      'react-hooks/exhaustive-deps': 'error' // Checks effect dependencies
+    }
   }
 );
