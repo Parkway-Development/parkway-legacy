a65fbf6987d399afa43d0396acfebcd4dfce1191
kylecreekmore <kyle.creekmore@gmail.com>
2024-06-03 23:43:28 -0400

    Display registration slots on the event display page

--- parkway.admin/src/components/events-page/EventDisplay.tsx
+++ parkway.admin/src/components/events-page/EventDisplay.tsx
@@ -5,6 +5,7 @@ import DateDisplay from '../date-display';
 import BooleanDisplay from '../boolean-display/BooleanDisplay.tsx';
 import EventCategoryDisplay from '../event-category-display';
 import TeamNameDisplay from '../team-name-display';
+import styles from './EventDisplay.module.css';
 
 const EventDisplay = (event: Event) => {
   const items: DescriptionsProps['items'] = [
@@ -40,16 +41,22 @@ const EventDisplay = (event: Event) => {
     },
     {
       key: 7,
+      label: 'Repeats',
+      // TODO: Fill out schedule details here
+      children: event.schedule ? 'Has schedule' : 'No'
+    },
+    {
+      key: 8,
       label: 'Location',
       children: event.location
     },
     {
-      key: 8,
+      key: 9,
       label: 'Category',
       children: <EventCategoryDisplay eventCategory={event.category} />
     },
     {
-      key: 9,
+      key: 10,
       label: 'Teams',
       children: event.teams?.length ? (
         <ul>
@@ -60,17 +67,68 @@ const EventDisplay = (event: Event) => {
           ))}
         </ul>
       ) : null
+    },
+    {
+      key: 11,
+      label: 'Allow Registrations',
+      children: <BooleanDisplay value={event.allowRegistrations} />
     }
   ];
 
+  let registrationSlots = null;
+  const slotsToDisplay = event.registrationSlots?.filter(
+    (slot) => !slot.deleted
+  );
+
+  if (event.allowRegistrations && slotsToDisplay?.length) {
+    const rows = slotsToDisplay.map((input) => {
+      return (
+        <tr key={input.slotId}>
+          <td>{input.name}</td>
+          <td>{input.description}</td>
+          <td>
+            <DateDisplay date={input.start} displayTime />
+          </td>
+          <td>
+            <DateDisplay date={input.end} displayTime />
+          </td>
+          <td>
+            <BooleanDisplay value={input.available} />
+          </td>
+        </tr>
+      );
+    });
+
+    registrationSlots = (
+      <div className={styles.registrationSlotContainer}>
+        <h3>Registration Slots: ({slotsToDisplay.length})</h3>
+        <table className={styles.registrationSlots}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th className={styles.dateColumn}>Start</th>
+              <th className={styles.dateColumn}>End</th>
+              <th className={styles.openColumn}>Open</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    );
+  }
+
   return (
-    <Descriptions
-      size="small"
-      title={event.name}
-      items={items}
-      bordered
-      column={1}
-    />
+    <>
+      <Descriptions
+        size="small"
+        title={event.name}
+        items={items}
+        bordered
+        column={1}
+      />
+      {registrationSlots}
+    </>
   );
 };
 
